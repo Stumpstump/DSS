ba0a9a87b3f361d3d855ee1cd48b889c82bd0aa6
Raymond <stump865@gmail.com>
2018-09-23 22:41:16 -0500

    Created a test scene and set up some basic AI to test the spawning.

---
+++ DSS/Assets/DSS Assets/EnemyNav.cs
@@ -0,0 +1,38 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyNav : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}*/
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNav : MonoBehaviour {
+
+    public Transform player;
+    public NavMeshAgent agent;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+}
